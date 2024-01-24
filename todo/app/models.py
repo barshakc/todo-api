1de7a365c19b1abbe9a6f72968ca3677a28b1fe5
Barsha <kcbarsha78@gmail.com>
2024-01-24 16:38:53 +0545

    Add TimeStampedModel

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    created=models.DateTimeField(auto_now_add=True)
+    modified=models.DateTimeField(auto_now=True)
+
+
+class ToDo(TimeStampedModel):
+    STATUS_CHOICES = [
+        ('todo', 'To do'),
+        ('in_progress', 'In progress'),
+        ('completed', 'Completed'),
+    ]
+
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    due_date=models.DateField()
+    status=models.CharField(max_length=255,choices=STATUS_CHOICES)
+    completion_date=models.DateField()
+
+    def __str__(self):
+        return self.title
