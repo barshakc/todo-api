e2ffca521198ecae35e774344c2e3c90e6cd76a8
Barsha <kcbarsha78@gmail.com>
2024-01-25 20:57:40 +0545

    Add CustomUser

--- todo/app/admin.py
+++ todo/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from app.models import CustomUser,ToDo
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(ToDo)
--- todo/app/models.py
+++ todo/app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser,Group,Permission
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+    
+
 class TimeStampedModel(models.Model):
     created=models.DateTimeField(auto_now_add=True)
     modified=models.DateTimeField(auto_now=True)
